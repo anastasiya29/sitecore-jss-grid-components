db34471ad3f369f4535b74516dea502c6626719b
Anastasiya Flynn <anastasiyar29@gmail.com>
2019-03-04 11:19:32 -0500

    create new app

---
+++ src/Layout.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Placeholder } from '@sitecore-jss/sitecore-jss-react';
+import Helmet from 'react-helmet';
+
+import './assets/app.css';
+
+/*
+  APP LAYOUT
+  This is where the app's HTML structure and root placeholders should be defined.
+
+  All routes share this root layout by default (this could be customized in RouteHandler),
+  but components added to inner placeholders are route-specific.
+*/
+const Layout = ({ route }) => (
+  <React.Fragment>
+    {/* react-helmet enables setting <head> contents, like title and OG meta tags */}
+    <Helmet>
+      <title>
+        {(route.fields && route.fields.pageTitle && route.fields.pageTitle.value) || 'Page'}
+      </title>
+    </Helmet>
+
+    {/* root placeholder for the app, which we add components to using route data */}
+    <div className="container">
+      <Placeholder name="jss-main" rendering={route} />
+    </div>
+  </React.Fragment>
+);
+
+export default Layout;
